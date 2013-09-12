460c9179dbf0b765c5c0b09aff1f3c194e68acdc
mochizuki <mochizuki@enrapt.jp>
2013-09-12 16:26:52 +0900

    add example page

---
+++ src/main/java/jp/enrapt/msgpackdemo/controllers/ExampleController.java
@@ -0,0 +1,13 @@
+package jp.enrapt.msgpackdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ExampleController {
+  @RequestMapping("/example")
+  public String example() {
+    return "example";
+  }
+}
---
+++ src/main/webapp/WEB-INF/jsp/example.jsp
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>example</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="/js/example.js"></script>
+  </head>
+  <body>
+    <h2>example</h2>
+    <div id="result"></div>
+  </body>
+</html>
