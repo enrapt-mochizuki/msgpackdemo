c9e1b38cdc4bd3adf7e9e3be780f418babe88044
mochizuki <mochizuki@enrapt.jp>
2013-09-18 14:04:38 +0900

    fix for IE9

--- src/main/webapp/WEB-INF/js/example.js
+++ src/main/webapp/WEB-INF/js/example.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){
-  $.ajaxSetup({
-    "beforeSend": function(xhr) {
-      xhr.overrideMimeType("text/plain; charset=x-user-defined");
-    },
-    "converters": {
-      '* msgpack': function(packed) {
-        return msgpack.unpack(packed);
-      }
-    },
-  });
-
-  $.get("/msgpackdemo", function(data) {
-    var text = "Customer: name = " + data.name + ", age = " + data.age;
+  msgpack.download("/msgpackdemo", {}, function(data) {
+    var text = "Customer: name " + data.name + ", age = " + data.age;
     $("#result").text(text);
-  }, "msgpack");
+  });
 });
