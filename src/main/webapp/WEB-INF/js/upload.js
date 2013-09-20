59faf5ea90c64e1f553d5e4df27ffa7fba0f1069
mochizuki <mochizuki@enrapt.jp>
2013-09-20 14:39:38 +0900

    add msgpack-send data type
    msgpack-send data type does not represent response data format.
    It's only used for hooking by prefilter.

--- src/main/webapp/WEB-INF/js/upload.js
+++ src/main/webapp/WEB-INF/js/upload.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $.ajaxPrefilter("msgpack-send", function(options) {
+    var toString = true;
+    var packed = msgpack.pack(options.data, toString);
+    var encoded = btoa(packed)
+    options.data = encoded;
+  });
+
+  $.ajaxSetup({
+    "converters": {
+      "* msgpack-send": true
+    }
+  });
+
   $("#submit").click(function() {
     var data = $("#text").val();
-    msgpack.upload("/api/upload", { "data": data }, function(text) {
+    $.post("/api/upload", data, function(text) {
       $("#result").text(text);
-    });
+    }, "msgpack-send");
   });
 });
